6e6ff5f3c85aeabc392525aaa9f8467a91017b20
KollerVMEscritorio <joseluiskoller98@gmail.com>
2023-06-27 15:21:34 -0300

    Cambios

---
+++ src/main/java/com/mongodb/quickstart/HelloMongoDB.java
@@ -0,0 +1,10 @@
+package com.mongodb.quickstart;
+
+public class HelloMongoDB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello MongoDB!");
+	}
+
+}
