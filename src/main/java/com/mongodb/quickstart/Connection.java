28e0bfb3aba5f7858e41d5d139d5da9d0eff6904
KollerVMEscritorio <joseluiskoller98@gmail.com>
2023-07-02 09:58:51 -0300

    Mongo conectado e imprimiendo las cosas

--- src/main/java/com/mongodb/quickstart/Connection.java
+++ src/main/java/com/mongodb/quickstart/Connection.java
@@ -2,6 +2,10 @@ package com.mongodb.quickstart;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
 import org.bson.Document;
 
 import java.util.ArrayList;
@@ -10,10 +14,18 @@ import java.util.List;
 public class Connection {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        String connectionString = System.getProperty("mongodb://localhost:27017");
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+        String connectionString = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)){
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
+            MongoDatabase database = mongoClient.getDatabase("tpo");
+            MongoCollection<Document> collection = database.getCollection("cc");
+            FindIterable<Document> documents = collection.find();
+            MongoCursor<Document> cursor = documents.iterator();
+            while (cursor.hasNext()) {
+                Document document = cursor.next();
+                System.out.println(document.toJson());
+            }
         }
     }
 }
