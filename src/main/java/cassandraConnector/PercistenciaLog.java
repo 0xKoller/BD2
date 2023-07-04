709baf93772d2dda5206ce936cf4e11e9be8055c
Seba VM <bernaseba1@gmail.com>
2023-07-04 08:24:23 -0700

    cassandra conectada a la bd del log, puede hacer inserts y select *. Ya indexe el campo del producto para que haga select solo por producto especifico, faltaria el metodo para la consulta esa

--- src/main/java/cassandraConnector/PercistenciaLog.java
+++ src/main/java/cassandraConnector/PercistenciaLog.java
@@ -1,5 +1,7 @@
 package cassandraConnector;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 
@@ -9,37 +11,62 @@ import java.util.Optional;
 import static java.lang.System.out;
 
 public class PercistenciaLog {
-    private final CassandraConnector client = new CassandraConnector();
 
-    public PercistenciaLog(final String newHost, final int newPort)
-    {
-        out.println("Connecting to IP Address " + newHost + ":" + newPort + "...");
-        client.connect(newHost, newPort);
+    private  Cluster cluster;
+    private Session session;
+
+    public PercistenciaLog(String node, int port) {
+        cluster = Cluster.builder().addContactPoint(node).withPort(port).build();
+        session = cluster.connect();
     }
 
-    public  void insertLogCarrito(final int idProd, final String campoMod,final String antes,
-                                 final String despues,final int idAdmin ) {
-        client.getSession().execute("INSERT INTO log_catalogo(id_producto,campo_modificado,antes,despues,id_admin" +
-                ")VALUES(?,?,?,?,?)", idProd, campoMod, antes, despues, idAdmin);
+    public void insertLog(int idProd, String campo, String antes, String despues, int idAdmin) {
+        String query = "INSERT INTO tpo.log_catalogo (id_producto,campo_modificado,antes,despues,id_admin) " +
+                "VALUES (?, ?, ?, ?, ?)";
 
-        System.out.println("Fila insertada");
-    }
-    /**
-     * Close my underlying Cassandra connection.
-     */
-    private void close(){
-        client.close();
+        session.execute(query, idProd, campo, antes, despues, idAdmin);
+
+        System.out.println("log successfully updated!");
     }
 
+    public void selectAllLog(){
+        String keyspace = "tpo;";
+        session.execute("USE " + keyspace);
+        String query = "SELECT * FROM log_catalogo;";
+        ResultSet resultSet = session.execute(query);
 
+
+        // Recorrer los resultados y mostrarlos en la consola
+        for (Row row : resultSet) {
+            // Obtener los valores de cada columna
+            int columna1 = row.getInt("id_producto");
+            String columna2 = row.getString("campo_modificado");
+            String columna3 = row.getString("antes");
+            String columna4 = row.getString("despues");
+            int columna5 = row.getInt("id_admin");
+
+
+            // Mostrar los valores en la consola
+            System.out.println("idProducto: " + columna1 + "   campo modificado: " + columna2 + "   antes: " + columna3 +
+                    "   despues: " + columna4 + "   idAdmin: " + columna5);
+        }
+    }
+
+    public void close() {
+        session.close();
+        cluster.close();
+    }
 
     public static void main(String[] args) {
-        int id = 10;
-        String campo = "precio";
-        String antes = "100";
-        String despues = "90";
-        int admin = 1909756;
+        String ipAddress = "127.0.0.1";
+        int port = 9042;
 
-       
+        /*PercistenciaLog insert = new PercistenciaLog(ipAddress, port);
+        insert.insertLog(44, "Precio", "100", "90", 1146056);
+        insert.close();*/
+
+        PercistenciaLog select = new PercistenciaLog(ipAddress,port);
+        select.selectAllLog();
+        select.close();
     }
 }
