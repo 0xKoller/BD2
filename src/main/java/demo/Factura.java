30ec74c2722bd5b978373dd81d7468ad7a110dc3
KollerVMEscritorio <joseluiskoller98@gmail.com>
2023-07-05 19:29:32 -0300

    Proba ahora

--- src/main/java/demo/Factura.java
+++ src/main/java/demo/Factura.java
@@ -12,6 +12,7 @@ public class Factura {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String id_user;
     private int doc;
     private double importe;
