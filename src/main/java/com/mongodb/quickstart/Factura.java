a0842fa68cec342aa2e13619fc8428efb6bda43e
Seba VM <bernaseba1@gmail.com>
2023-07-13 19:12:48 -0700

    cambio main

--- src/main/java/com/mongodb/quickstart/Factura.java
+++ src/main/java/com/mongodb/quickstart/Factura.java
@@ -1,25 +1,18 @@
-package com.mongodb.quickstart;
-
+package  com.mongodb.quickstart;
 import com.mongodb.client.*;
-import it.unimi.dsi.fastutil.Hash;
 import org.bson.Document;
 
 import java.util.*;
 
 public class Factura {
 
-    private static String id;
-
-    private static  String id_user;
-    private  static double importe;
-    private static  Document productos;
-    private  static boolean metodo;
-    private  static String id_emple;
-    private static  Date fecha;
-
-    private boolean estado;
-
-
+    private String id;
+    private String id_user;
+    private double importe;
+    private Document productos;
+    private boolean metodo;
+    private String id_emple;
+    private Date fecha;
 
     public Factura() {
         this.id_user = "";
@@ -39,27 +32,24 @@ public class Factura {
         this.fecha = new Date();
     }
 
-    public static String getId() {
+    public String getId() {
         return id;
     }
 
-    private static String generarId(){
+    private static String generarId() {
         Random random = new Random();
         String caracteres = "0123456789";
         int longitud = 10;
-        String nuevoId = "";
-            StringBuilder sb = new StringBuilder(longitud);
-            for (int i = 0; i < longitud; i++) {
-                int indice = random.nextInt(caracteres.length());
-                char caracter = caracteres.charAt(indice);
-                sb.append(caracter);
-            }
-            nuevoId = sb.toString();
-
-        return nuevoId;
+        StringBuilder sb = new StringBuilder(longitud);
+        for (int i = 0; i < longitud; i++) {
+            int indice = random.nextInt(caracteres.length());
+            char caracter = caracteres.charAt(indice);
+            sb.append(caracter);
+        }
+        return sb.toString();
     }
 
-    public  static String getId_user() {
+    public String getId_user() {
         return id_user;
     }
 
@@ -67,7 +57,7 @@ public class Factura {
         this.id_user = id_user;
     }
 
-    public  static double getImporte() {
+    public double getImporte() {
         return importe;
     }
 
@@ -75,13 +65,10 @@ public class Factura {
         this.importe = importe;
     }
 
-
-    public HashMap<String, Object> getProductos(){
-        HashMap<String, Object> map = new HashMap<String, Object>(this.productos);
+    public HashMap<String, Object> getProductos() {
+        HashMap<String, Object> map = new HashMap<>(this.productos);
         return map;
     }
-
-
 
     public void setProductos(HashMap<String, Object> productos) {
         float resultado = 0;
@@ -90,16 +77,14 @@ public class Factura {
             resultado += floatValue;
         }
         setImporte(resultado);
-        Document doc = new Document(productos);
-        this.productos = doc;
+        this.productos = new Document(productos);
     }
 
-    public Document getProductosForFactura(){
-        Document productos = new Document(this.productos);
-        return productos;
+    public Document getProductosForFactura() {
+        return new Document(this.productos);
     }
 
-    public static  boolean getMetodo() {
+    public boolean getMetodo() {
         return metodo;
     }
 
@@ -107,15 +92,15 @@ public class Factura {
         this.metodo = metodo;
     }
 
-    public  static String getId_emple() {
+    public String getId_emple() {
         return id_emple;
     }
 
-    public  static void setId_emple(String id_emple) {
-        Factura.id_emple = id_emple;
+    public void setId_emple(String id_emple) {
+        this.id_emple = id_emple;
     }
 
-    public  static Date getFecha() {
+    public Date getFecha() {
         return fecha;
     }
 
